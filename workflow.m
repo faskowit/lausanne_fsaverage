cb594c0c65dfb03ed04a9e5112659a4e41311319
faskowit <jfaskowi@iu.edu>
2018-07-15 11:08:13 -0400

    changed so that lh and rh use one annotation

--- workflow.m
+++ workflow.m
@@ -2,7 +2,7 @@
 
 addpath(genpath('/home/jfaskowi/JOSHSTUFF/software/FS_6p0/freesurfer/matlab/')) 
 
-scale = '250' ;
+scale = '60' ;
 
 %% first get the names  of the files I want
 
@@ -21,6 +21,38 @@ rh_filenames = cell(length(rh_files),1) ;
 % read one file to get started here
 [ lh_temp_vert , lh_temp_lab , lh_temp_ct ] = read_annotation([lh_files(1).folder '/' lh_files(1).name]) ;
 [ rh_temp_vert , rh_temp_lab , rh_temp_ct ] = read_annotation([rh_files(1).folder '/' rh_files(1).name]) ;
+
+%% make new combined colortable 
+
+lh_ct = lh_temp_ct.table ;
+rh_ct = rh_temp_ct.table ;
+
+new_rh_ct = zeros(size(rh_temp_ct.table)) ;
+new_rh_ct(1,:) = rh_ct(1,:) ;
+
+for idx = 2:size(rh_ct) 
+    
+    currRow = rh_ct(idx,:) ;
+    tmpMatch = currRow(5) == lh_ct(:,5) ;
+
+    rowAdds = [ 1 0 0 ;
+       0 1 0 ;
+       0 0 1 ] ;
+    rowAddIdx = 0 ;
+    while sum(tmpMatch) > 0
+    
+        disp([ 'new color:' num2str(idx) ])
+        
+        currRow(1:3) = currRow(1:3) + rowAdds(mod(rowAddIdx,3)+1,:) ;
+        rowAddIdx = rowAddIdx + 1;
+        currRow(5) = currRow(1) + currRow(2)*2^8 + currRow(3)*2^16 ;
+        
+        tmpMatch = currRow(5) == lh_ct(:,5) ;
+    end
+
+    new_rh_ct(idx,:) = currRow ;
+    
+end
 
 %% fix annotation to match information from website
 
@@ -115,6 +147,14 @@ end
 lh_lab_mode = mode(lh_lab_data,2) ;
 rh_lab_mode = mode(rh_lab_data,2) ;
 
+%% change the color vals for the rh_lab_data
+
+for idx = 2:length(new_rh_ct)
+    
+    rh_lab_mode(rh_lab_mode == rh_ct(idx,5)) = new_rh_ct(idx,5) ;
+
+end
+    
 %% write out annotation 
 
 % function write_annotation(filename, vertices, label, ct)
@@ -122,20 +162,36 @@ rh_lab_mode = mode(rh_lab_data,2) ;
 lh_annot_name = [pwd '/lh.myatlas' scale '.annot' ] ;
 rh_annot_name = [pwd '/rh.myatlas' scale '.annot' ] ;
 
-new_lh_temp_ct = lh_temp_ct ;
-new_rh_temp_ct = rh_temp_ct ;
+% new_lh_temp_ct = lh_temp_ct ;
+% new_rh_temp_ct = rh_temp_ct ;
+% 
+% new_lh_temp_ct.struct_names = lh_temp_ct.struct_names(lh_reorder) ;
+% new_rh_temp_ct.struct_names = rh_temp_ct.struct_names(rh_reorder) ;
+% 
+% new_lh_temp_ct.table = lh_temp_ct.table(lh_reorder,:) ;
+% new_rh_temp_ct.table = rh_temp_ct.table(rh_reorder,:) ;
+% 
+% new_lh_temp_ct.numEntries = size(new_lh_temp_ct.table,1) ;
+% new_rh_temp_ct.numEntries = size(new_rh_temp_ct.table,1) ;
 
-new_lh_temp_ct.struct_names = lh_temp_ct.struct_names(lh_reorder) ;
-new_rh_temp_ct.struct_names = rh_temp_ct.struct_names(rh_reorder) ;
+% write with just one annotation
+combo_names = [ lh_temp_ct.struct_names(1,:) ;
+    cellfun(@(x) [ 'LH_' x ] , lh_temp_ct.struct_names(lh_reorder(2:end)),'UniformOutput',0) ;
+    cellfun(@(x) [ 'RH_' x ] , rh_temp_ct.struct_names(rh_reorder(2:end)),'UniformOutput',0) ] ;
 
-new_lh_temp_ct.table = lh_temp_ct.table(lh_reorder,:) ;
-new_rh_temp_ct.table = rh_temp_ct.table(rh_reorder,:) ;
+combo_ct = [ lh_temp_ct.table(lh_reorder,:) ;
+    new_rh_ct(rh_reorder(2:end) ,:) ] ;
 
-new_lh_temp_ct.numEntries = size(new_lh_temp_ct.table,1) ;
-new_rh_temp_ct.numEntries = size(new_rh_temp_ct.table,1) ;
+numEntries = size(combo_ct,1) ;
 
-write_annotation(lh_annot_name,lh_temp_vert,lh_lab_mode,new_lh_temp_ct)
-write_annotation(rh_annot_name,rh_temp_vert,rh_lab_mode,new_rh_temp_ct)
+write_ct = struct();
+write_ct.numEntries = numEntries ;
+write_ct.orig_tab = 'made by j faskowitz' ;
+write_ct.struct_names = combo_names ;
+write_ct.table = combo_ct ;
+
+write_annotation(lh_annot_name,lh_temp_vert,lh_lab_mode,write_ct)
+write_annotation(rh_annot_name,rh_temp_vert,rh_lab_mode,write_ct)
 
 
 
